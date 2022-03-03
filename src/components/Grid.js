f5db48b6f4ca9e9cbf60c68da977acf99a5d20cc
tom ames <tsames1@gmail.com>
2022-03-03 15:54:40 -0800

    Continued work on user input grids

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,67 +6,91 @@ import '../styles/grid.scss';
 
 const Grid = (props) => {
 
-  const [mo, setMo] = useState(false);
-
-  const handleMouseEnter = () => {
-    setMo(true);
+  const addActiveClass = (e) => {
+    e.target.className = " gridCell gridActive"
   }
 
-  const handleMouseLeave = () => {
-    setMo(false)
+  const removeActiveClass = (e) => {
+    e.target.className = "gridCell";
   }
 
   return (
-    <table>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-      <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-      </tr>
-    </table>
+    <div className="gridTable">
+      <div className="gridContents">
+        <div id="1" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+        <div id="2" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+        <div id="3" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+        <div id="4" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+        <div id="5" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+        <div id="6" className="gridColumn">
+          <div id="1" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="2" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="3" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="4" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="5" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="6" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="7" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="8" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="9" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+          <div id="10" className="gridCell" onMouseEnter={addActiveClass} onMouseLeave={removeActiveClass}></div>
+        </div>
+      </div>
+    </div>
   );
 };
 

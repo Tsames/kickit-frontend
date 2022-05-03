144ddaf24914c488810f800226f98dd1e96d3438
tom ames <tsames1@gmail.com>
2022-05-03 15:39:10 -0700

    added footer anchor tags and styled correctly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,18 @@ import '../styles/footer.scss';
 
 const Footer = ({ form, type, name, text, value, doThis }) => {
 
+  const handleMouseEnter = (event) => {
+    event.target.classList.add("mouseover");
+  }
+
+  const handleMouseLeave = (event) => {
+    event.target.classList.remove("mouseover");
+  }
+
   return (
     <div className="pageFooter">
-      <a src="https://github.com/Tsames/kickit-frontend">github</a>
+      <a id="footer-left-side" href="https://github.com/Tsames/kickit-frontend" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>github</a>
+      <p id="footer-right-side">created by <a href="https://www.linkedin.com/in/thomasames/" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Tom Ames</a></p>
     </div>
   )
 }

4bab466b0a20d5d7fd78d733367785a00cfcfdc5
tom ames <tsames1@gmail.com>
2022-09-28 16:25:24 -0700

    Added Content Text to About.js

--- src/components/page_components/About.js
+++ src/components/page_components/About.js
@@ -31,15 +31,14 @@ const About = ({ setRoot }) => {
 
   const aboutHow = () => {
     return (
-      <div id="aboutHow-block">
-
+      <>
         <div id="findEventBlock">
           <h3>Finding your event</h3>
           <p>
 
           Upon visiting Kick-It, a user will want to find their event's dedicated page.
           The first and easiest way to find your event is to use a direct link.
-          The organizer of your event, whether it be you or not, will receive a link upon creating an event that they can then distribute to all potential attendees.
+          The organizer of your event, whether it be you or another, will receive a link upon creating an event that they can then distribute to all potential attendees.
           Otherwise, if you've misplaced your link or never got one you can enter your event's unique id in the search bar on the home page.<br></br><br></br>
 
           <span id="noticeText">* Please note, if you are here just to view and test out the web app's functionality you'll want to click on the example option in the top right of your screen.</span>
@@ -49,47 +48,70 @@ const About = ({ setRoot }) => {
         <div id="signUpBlock">
           <h3>Signing Up</h3>
           <p>
-          To sign up for an event you'll click on the !
-          
-          
-          A user can sign up for the event by inputing the name they'd like to be listed under and entering the days and times that they are available.
-          Once an attendee has submitted their availability they can edit the date and times that they are available at by resubmitting under the same name.
+          Once you've found your events dedicated page you'll want to sign up for the event.
+          You can sign up for the event by first inputing the name you'd like to be listed under.
+          Then click and drag among the grid to highlight the times that you are available to attend the event.
+          Once you have submitted your availability you can edit the date and times that you previously submitted by resubmitting under the same name.
           However, an important consequence of this design is that any given attendee must submit their availability under a unique name.
           If they do not, their submission will overwrite another attendee's submission.
-          Kick-it does try and catch these potential mishaps by warning the user when the name they are submitting under is already taken.<br></br>
+          The Kick-it app does try and catch these potential mishaps by warning the user when the name they are submitting under is already taken and will replace existing data.<br></br>
           </p>
         </div>
 
         <div id="viewingBlock">
           <h3>Viewing Availability</h3>
           <p>
-          Or, alternatively, they can view the availability of everyone who has signed up for the event thus far.
-          When viewing the sign ups for the event one can 
+            Great, now that you've entered your own availability you'll be curious to view everyone elses'.
+            
           </p>
         </div>
 
-      </div>
+        <div id="createEventBlock">
+          <h3>Creating your own Event</h3>
+          <p>
+            Creating your own event on Kick it is very simple.
+            Click on the 'create' option in the top right of your screen.
+            You'll be asked to input a title, location, description, time range, and potential days for your event.
+            Choose carefully when entering these details, as of the first iteration of kick it there are no accounts.
+            This means you do not have to create an account to make your own event.
+            It also means that you don't have the luxury of editting your event once it has been created.
+          </p>
+        </div>
+
+        <div id="distributingEventBlock">
+          <h3>Sharing your Event</h3>
+          <p>
+            Great
+          </p>
+        </div>
+      </>
     )
   }
 
   const aboutWho = () => {
     return (
-      <div id="aboutWho">
-        <div id="author-block">
+      <>
+        <div id="authorBlock">
           <h3>Author</h3>
           <p>
-          <span>Kick-it</span> was created by <a className="aboutLink" href="https://www.linkedin.com/in/thomasames">Tom Ames</a>.
+          <span id="smallBranding">Kick-it</span> was created by <a className="aboutLink" href="https://www.linkedin.com/in/thomasames">Tom Ames</a>.
           </p>
         </div>
-        <div id="techUsed-block">
+        <div id="techBlock">
           <h3>Tech Used</h3>
           <p>
-            Kick-it's front-end was created with React, React-router, and a lot of custom styling featuring Scss.
+            Kick-it's front-end was created with React, React-router, React Icons, and a lot of custom styling featuring Scss.
             The backend was created with Node, Express, Mongoose and MongoDB.
             If you are interested in taking a closer look at the code you can find the respective repositories for both the <a className="aboutLink" href="https://github.com/Tsames/kickit-frontend">frontend</a> and the <a className="aboutLink" href="https://github.com/Tsames/kickit-backend-node">backend</a> on Github.
           </p>
         </div>
-      </div>
+        <div id="futurefeaturesBlock">
+          <h3>Future Features</h3>
+          <p>
+
+          </p>
+        </div>
+      </>
     )
   }
 
@@ -97,12 +119,16 @@ const About = ({ setRoot }) => {
 
   return (
     <div id="about-shell">
-      <h1 id="welcome-heading">Welcome to Kick it</h1>
-      <div id="aboutSections">
-        <Link to="/about/how"><button id="toAboutHow" className="aboutClickables aboutSection">How its Works</button></Link>
-        <Link to="/about/who"><button id="toAboutWho" className="aboutClickables aboutSection">Author & Tech</button></Link>
+    <div id="about-top">
+        <h1 id="welcome-heading">Welcome to Kick it</h1>
+        <div id="aboutSections">
+          <Link to="/about/how"><button id="toAboutHow" className="aboutClickables aboutSection">How its Works</button></Link>
+          <Link to="/about/who"><button id="toAboutWho" className="aboutClickables aboutSection">Author & Tech</button></Link>
+        </div>
       </div>
-      { selectedPage === '/about/how' ? aboutHow() : aboutWho() }
+      <div id="about-body">
+        { selectedPage === '/about/how' ? aboutHow() : aboutWho() }
+      </div>
     </div>
   )
 }

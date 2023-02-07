80af256cf38710d59f23c46a3e28a5f0b658acad
tom <tsames1@gmail.com>
2023-02-06 19:11:36 -0800

    Making progress on new create page

--- src/components/create_pages/create_page/child_components/createTopSection.js
+++ src/components/create_pages/create_page/child_components/createTopSection.js
@@ -23,14 +23,14 @@ const CreateTopSection = ({newForm, handleChange}) => {
     <div id="create-top-section">
       <div id="create-top-left-subsection">
         <label htmlFor="title" id="create-title-wrapper" className="create-top-left-wrapper">
-          <input required id="create-title" className="create-top-left-field" type="text" name="title" value={newForm.title} onChange={handleChange} />
+          <input required id="create-title" className="create-top-left-field" type="text" name="title" value={newForm.title} placeholder="Enter Event Title..." onChange={handleChange} />
         </label>
         <label htmlFor="location" id="create-location-wrapper" className="create-top-left-wrapper">
-          <input required id="create-location" className="create-top-left-field" type="text" name="location" value={newForm.location} onChange={handleChange} />
+          <input required id="create-location" className="create-top-left-field" type="text" name="location" value={newForm.location} placeholder="Event Location..." onChange={handleChange} />
         </label>
       </div>
         <label htmlFor="description" id="create-description-wrapper" className="create-top-right-wrapper">
-          <textarea wrap="soft" required id="create-description" name="description" value={newForm.description} onChange={handleChange} />
+          <textarea wrap="soft" required id="create-description" name="description" value={newForm.description} placeholder="Enter a description of your event..." onChange={handleChange} />
         </label>
     </div>
   )

a99a8b9d4bc3e4ba2d46944624341af34ee028f2
tom <tsames1@gmail.com>
2023-02-23 10:32:30 -0800

    Making adjustments to home.tsx's stylesheets.

--- src/components/home_page/Home.tsx
+++ src/components/home_page/Home.tsx
@@ -56,8 +56,7 @@ const Home: FC<homeProps> = () => {
   //Hover
   const buttonItemHover = {
     scale: 1.3,
-    border: "solid",
-    borderColor: "#818DFF",
+    borderWidth: "max(0.4rem, 0.4vw)",
     transition: {
       duration: 0.2,
     }
@@ -96,25 +95,25 @@ const Home: FC<homeProps> = () => {
         <motion.button id="createEvent" variants={childVariant} whileHover={buttonItemHover} whileTap={buttonItemTap}>Create Your Event</motion.button>
       </motion.div>
       <div id="home-testimonials">
-        <div id="home-testimonial-one" className="home-testimonial-wrapper">
+        <div id="home-testimonial-one" className="no-select">
           <h1 id="home-testimonial-quote-one" className="home-testimonial-quote">"Kick It makes it so easy to hang with my pals. Scheduling sucks without it!"</h1>
           <p id="home-testimonial-speaker-one" className="home-testimonial-speaker">-Jimbo</p>
         </div>
       </div>
-      <div id="home-secondary"></div>
       <div id="home-contents">
+        <div id="home-secondary"></div>
         <div id="home-content-block-one">
           <div id="home-content-graphic-one" className="home-content-graphic"></div>
           <p id="home-content-text-one" className="home-content-text">Create an event and choose a few potential days and times for it.</p>
+          <motion.div id="home-content-graphic-two" className="home-content-graphic" variants={scrollVariant} initial="initial" whileInView="animate" viewport={{ once: true, amount: 0.8 }}></motion.div>
         </div>
-        <motion.div id="home-content-graphic-two" className="home-content-graphic" variants={scrollVariant} initial="initial" whileInView="animate" viewport={{ once: true, amount: 0.8 }}></motion.div>
         <div id="home-content-block-two">
           <div id="home-content-text-two-wrapper">
             <p id="home-content-text-two" className="home-content-text">Share the event with your friends and see when they are free.</p>
           </div>
           <div id="home-content-graphic-three" className="home-content-graphic"></div>
+          <motion.div id="home-content-graphic-four" className="home-content-graphic" variants={scrollVariant} initial="initial" whileInView="animate" viewport={{ once: true, amount: 0.8 }}></motion.div>
         </div>
-        <motion.div id="home-content-graphic-four" className="home-content-graphic" variants={scrollVariant} initial="initial" whileInView="animate" viewport={{ once: true, amount: 0.8 }}></motion.div>
         <div id="home-content-block-three">
           <div id="home-content-text-three-four-wrapper">
             <p id="home-content-text-three" className="home-content-text">Pick the best day and time for everyone! Its time to</p>

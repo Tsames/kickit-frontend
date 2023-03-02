a5bde27d62c4398811f2af8a6549156957ad9501
tom <tsames1@gmail.com>
2023-03-02 15:32:48 -0800

    Finished the first version of the details component.

--- src/components/event_pages/event_page/child_components/details.tsx
+++ src/components/event_pages/event_page/child_components/details.tsx
@@ -1,6 +1,7 @@
 //Dependencies
 import React, { FC, useState, useEffect } from "react";
 import { motion } from 'framer-motion';
+import { VscTriangleDown } from "react-icons/vsc";
 
 //Styling
 import '../../../../styles/events_styling/event_styling/child_components/details.scss';
@@ -40,11 +41,58 @@ const Details: FC<detailsProps> = ({ eventData }) => {
 
   /* ------------------------------------------ Animation Details (Framer-Motion) ------------------------------------------ */
 
+  const detailsShellVariant = {
+    inactive: {
+        transition: { duration: 0.3, delay: 0.3 }
+    },
+    active: {
+        transition: { duration: 0.3 }
+    }
+  }
+
+  const detailsHeaderVariant = {
+    inactive: {
+        backgroundColor: "#FAF9F6",
+        transition: { duration: 0.3, delay: 0.3 }
+    },
+    active: {
+        backgroundColor: "#C2C2C2",
+        transition: { duration: 0.3 }
+    }
+  }
+
+  const detailsBodyVariant = {
+    inactive: {
+      opacity: 0,
+      height: 0,
+      transition: { duration: 0.3, delay: 0.3 }
+    },
+    active: {
+      opacity: 1,
+      height: "calc(82% - (2rem + 2vw))",
+      transition: { duration: 0.3 }
+    }
+  }
+
+  const detailsToggleButtonVariant = {
+    inactive: {
+        rotateZ: "90deg",
+        transition: { duration: 0.3, delay: 0.3 }
+    },
+    active: {
+        rotateZ: "0deg",
+        transition: { duration: 0.3 }
+    }
+  }
 
   /* ------------------------------------------ Helper Functions ------------------------------------------ */
 
 
   /* ------------------------------------------ Event Handler Functions ------------------------------------------ */
+
+  const toggleDetails = () => {
+
+  }
 
   /* ------------------------------------------ Conditional JSX ------------------------------------------ */
 
@@ -52,18 +100,20 @@ const Details: FC<detailsProps> = ({ eventData }) => {
   /* ------------------------------------------ Returning JSX ------------------------------------------ */
 
     return (
-        <motion.div id="details-shell" className="">
-            <div id="event-details-title-wrapper">
-              <h3 id="event-details-title-label" className="event-details-label no-select">Title:</h3>
-              <h3 id="event-details-title" className="event-details">{eventData.title}</h3>
-            </div>
-            <div id="event-details-location-wrapper">
-              <h3 id="event-details-location-label" className="event-details-label no-select">Location:</h3>
-              <h3 id="event-details-location" className="event-details">{eventData.location}</h3>
-            </div>
-            <div id="event-details-description-wrapper">
-              <p id="event-details-description">{eventData.description !== "" ? eventData.description : "No event description was given."}</p>
-            </div>
+        <motion.div id="details-shell" variants={detailsShellVariant} initial={false} animate={toggle ? "active" : "inactive"}>
+            <motion.div id="details-header" variants={detailsHeaderVariant} initial={false} animate={toggle ? "active" : "inactive"}>
+                <h1 id="details-event-title">{eventData.title}</h1>
+                <motion.div id="details-toggle-button-wrapper" variants={detailsToggleButtonVariant} onClick={() => {toggle ? setToggle(false) : setToggle(true)}}><VscTriangleDown id="details-toggle-button"></VscTriangleDown></motion.div>
+            </motion.div>
+            <motion.div id="details-body" variants={detailsBodyVariant} initial={false} animate={toggle ? "active" : "inactive"}>
+                <div id="event-details-location-wrapper">
+                    <h3 id="event-details-location-label" className="event-details-label no-select">Location:</h3>
+                    <h3 id="event-details-location" className="event-details">{eventData.location}</h3>
+                </div>
+                <div id="event-details-description-wrapper">
+                    <p id="event-details-description">{eventData.description !== "" ? eventData.description : "No event description was given."}</p>
+                </div>
+            </motion.div>
         </motion.div>
     )
 }

b61f86c1d69f7b2463707bac70812362b177164b
tom <tsames1@gmail.com>
2023-03-14 15:45:48 -0700

    Ongoing styling changes.

--- src/components/navigation_components/navbar.tsx
+++ src/components/navigation_components/navbar.tsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect, useRef, FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 //Styling
 import '../../styles/navigation_styling/navbar.scss';
@@ -20,31 +22,47 @@ const Navbar: FC<navbarProps> = ({ eventId }) => {
 
   const location = useLocation().pathname;
 
+  const [toggle, setToggle] = useState(false);
+
   /* ------------------------------------------ Animation Details (Framer-Motion) ------------------------------------------ */
 
   //Container (navbar-shell) Variant
   const containerVariant = {
-    initial: { 
+    hidden: { 
       width: 0,
       height: "100%"
     },
-    entranceAnimate: {
+    visible: {
       width: "100%",
       height: "100%",
       transition: {
         duration: 0.6,
-        delay: 1,
         when: "beforeChildren" 
+      }
+    }
+  }
+
+  const extendedMenuVariant = {
+    hidden: {
+      height: 0,
+      transition: {
+        type: "spring"
+      }
+    },
+    visible: {
+      height: "15vw",
+      transition: {
+        type: "spring"
       }
     }
   }
 
   //Child Variants
   const childVariant = {
-    initial: { 
+    hidden: { 
       opacity: 0 
     },
-    entranceAnimate: {
+    visible: {
       opacity: 1,
       transition: {
         duration: 0.6
@@ -84,25 +102,62 @@ const Navbar: FC<navbarProps> = ({ eventId }) => {
 
   /* ------------------------------------------ Conditional JSX ------------------------------------------ */
 
+  //Conditional JSX for Desktops
+  const desktop = () => {
+    return (
+      <div id="navRight">
+        <div id="navItems-wrapper">
+          <Link to={`/event/${eventId}`} className={location.slice(0, 7) === "/event/" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarEvent" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>{eventId === "example" ? "Example Event" : "Your Event"}</motion.button></Link>
+          <Link to="/howItWorks" className={location === "/howItWorks" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarHowItWorks" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>How It Works</motion.button></Link>
+          <Link to="/aboutUs" className={location === "/aboutUs" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarAboutUs" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>About Us</motion.button></Link>
+        </div>
+        <div id="navCreate-wrapper">
+          <Link to="/create" id="navbarCreateLink" className={location === "/create" ? "onPage no-select" : ""}><motion.button id="navbarCreate" variants={childVariant} whileHover={createButtonHover} whileTap={createButtonTap}>Create</motion.button></Link>
+        </div>
+      </div>
+    )
+  }
+
+  //Conditional JSX for Mobile and Portrait Tablets
+  const mobile = () => {
+    return (
+      <>
+        <motion.div id="navRight">
+          <Link to="/create"><motion.button id="navMobileCreateButton" variants={childVariant} initial="hidden" animate={toggle ? "visible" : "hidden"}><AiFillPlusCircle id="navMobileCreateIcon"></AiFillPlusCircle></motion.button></Link>
+        </motion.div>
+        <motion.div id="navMiddle">
+          <motion.button id="navMobileButton" variants={childVariant} initial="hidden" animate="visible" onClick={() => setToggle(!toggle)}><HiMenuAlt3 id="navMobileMenuIcon"></HiMenuAlt3></motion.button>
+        </motion.div>
+      </>
+    )
+  }
+
+  const extended = () => {
+    return (
+      <motion.div id="navExtended" variants={extendedMenuVariant} initial={false} animate={ toggle ? "visible" : "invisible"}>
+        <div id="navItems-wrapper">
+          <Link to={`/event/${eventId}`} className={location.slice(0, 7) === "/event/" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarEvent" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>{eventId === "example" ? "Example Event" : "Your Event"}</motion.button></Link>
+          <Link to="/howItWorks" className={location === "/howItWorks" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarHowItWorks" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>How It Works</motion.button></Link>
+          <Link to="/aboutUs" className={location === "/aboutUs" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarAboutUs" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>About Us</motion.button></Link>
+        </div>
+        <div id="navCreate-wrapper">
+          <Link to="/create" id="navbarCreateLink" className={location === "/create" ? "onPage no-select" : ""}><motion.button id="navbarCreate" variants={childVariant} whileHover={createButtonHover} whileTap={createButtonTap}>Create</motion.button></Link>
+        </div>
+    </motion.div>
+    )
+  }
+
   /* ------------------------------------------ Returning JSX ------------------------------------------ */
 
   return (
     <nav id="navbar-shell">
-      <motion.div id="navbar-motion-wrapper" variants={containerVariant} initial="initial" animate="entranceAnimate">
+      <motion.div id="navbar-motion-wrapper" variants={containerVariant} initial="hidden" animate="visible">
         <div id="navLeft">
-          <Link to="/" id="navbarHomeLink" className={location === "/" ? "onPage no-select" : ""}><Logo></Logo></Link>
+          <Link to="/" id="navbarHomeLink" className={location === "/" ? "onPage no-select" : ""}><button id="navbarHomeButton">Kick It</button></Link>
         </div>
-        <div id="navRight">
-          <div id="navItems-wrapper">
-            <Link to={`/event/${eventId}`} className={location.slice(0, 7) === "/event/" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarEvent" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>{eventId === "example" ? "Example Event" : "Your Event"}</motion.button></Link>
-            <Link to="/howItWorks" className={location === "/howItWorks" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarHowItWorks" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>How It Works</motion.button></Link>
-            <Link to="/aboutUs" className={location === "/aboutUs" ? "onPage navBarLink no-select" : "navBarLink"}><motion.button id="navbarAboutUs" className="navItem" variants={childVariant} whileHover={navItemHover} whileTap={navItemTap}>About Us</motion.button></Link>
-          </div>
-          <div id="navCreate-wrapper">
-            <Link to="/create" id="navbarCreateLink" className={location === "/create" ? "onPage no-select" : ""}><motion.button id="navbarCreate" variants={childVariant} whileHover={createButtonHover} whileTap={createButtonTap}>Create</motion.button></Link>
-          </div>
-        </div>
+        { window.innerWidth >= 900 ? desktop() : mobile()}
       </motion.div>
+      { window.innerWidth >= 900 ? null : extended() }
     </nav>
   );
 };
